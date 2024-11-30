2a9b7559d0141fa2efab38ae5aa8bbb66600d2bd
Jan <janpleszynski@gmail.com>
2024-11-30 12:51:23 +0100

    Change naming to correspond to our domain.

--- chains.py
+++ chains.py
@@ -9,16 +9,28 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 openai_model = os.getenv("OPENAI_MODEL")
 
 llm = ChatOpenAI(model=openai_model, api_key=openai_api_key)
-review_prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a company representative reviewing a customer's complaint. "
-        "Analyze the complaint details and provide a thorough assessment."),
+subjects = ["process", "benefits", "details", "other"]
+inquiry_classification = ChatPromptTemplate.from_messages([
+    ("system", """
+    You are an office assistant responsible for responding to user messages. 
+Based on the provided input, determine the topic of conversation. 
+The available topics are:
+- HR process details (Keyword: process)
+- Benefits (Keyword: benefits)
+- Employment details (Keyword: details)
+- Other (Keyword: other)
+
+Please respond with a single word keyword that corresponds to the identified topic.
+    """),
     MessagesPlaceholder(variable_name="messages"),
 ])
-response_prompt = ChatPromptTemplate.from_messages([
+
+
+inquiry_response = ChatPromptTemplate.from_messages([
     ("system", "You are a company AI assistant tasked with drafting responses to customer complaint. "
         "Generate a professional and empathetic response to the customer's complaint."),
     MessagesPlaceholder(variable_name="messages"),
 ])
 
-generate_response_chain = response_prompt | llm
-review_complaint_chain = review_prompt | llm
+generate_classify_inquiry_chain = inquiry_classification | llm
+generate_inquiry_response_chain = inquiry_response | llm
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from typing import List, Sequence
 from langchain_core.messages import BaseMessage, HumanMessage
 from langgraph.graph import END, MessageGraph
-from chains import generate_response_chain, review_complaint_chain
+from chains import generate_inquiry_response_chain, generate_classify_inquiry_chain
 
 
-def response_node(state: Sequence[BaseMessage]):
-    return generate_response_chain.invoke({"messages": state})
+def classify_inquiry_node(state: Sequence[BaseMessage]):
+    return generate_classify_inquiry_chain.invoke({"messages": state})
 
 
-def review_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
-    res = review_complaint_chain.invoke({"messages": messages})
+def inquiry_response_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+    res = generate_inquiry_response_chain.invoke({"messages": messages})
     return [HumanMessage(content=res.content)]
 
 
@@ -20,23 +20,21 @@ def should_continue(state: List[BaseMessage]):
 
 
 builder = MessageGraph()
-builder.add_node("RESPOND", response_node)
-builder.add_node("REVIEW", review_node)
-builder.set_entry_point("REVIEW")
+builder.add_node("CLASSIFY", classify_inquiry_node)
+builder.add_node("RESPOND", inquiry_response_node)
+builder.set_entry_point("CLASSIFY")
 
 
-builder.add_conditional_edges("REVIEW", should_continue)
-builder.add_edge("RESPOND", "REVIEW")
+builder.add_conditional_edges("CLASSIFY", should_continue)
+builder.add_edge("CLASSIFY", "RESPOND")
 graph = builder.compile()
 
 
 if __name__ == "__main__":
-    print("Complaint Response Generator") 
+    print("Faq Responder")
     inputs = HumanMessage(content="""
         Customer Complaint:
-        I don’t recognize a charge of $299.99 for ‘TechGadget’ on my statement dated July 15, 2023. 
-        I’ve never bought this product and I believe this charge is fraudulent. 
-        Please investigate and remove this charge from my account.
+        Please tell me what benefits can I get in the company?
     """)
     response = graph.invoke(inputs)
     print(response)
