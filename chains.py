2438ded2cfc5bdd1f554003ed35ab449dd7a7ef1
Jan <janpleszynski@gmail.com>
2024-11-30 13:42:05 +0100

    Move prompts to files.

--- chains.py
+++ chains.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
@@ -11,24 +13,14 @@ openai_model = os.getenv("OPENAI_MODEL")
 llm = ChatOpenAI(model=openai_model, api_key=openai_api_key)
 subjects = ["process", "benefits", "details", "other"]
 inquiry_classification = ChatPromptTemplate.from_messages([
-    ("system", """
-    You are an office assistant responsible for responding to user messages. 
-Based on the provided input, determine the topic of conversation. 
-The available topics are:
-- HR process details (Keyword: process)
-- Benefits (Keyword: benefits)
-- Employment details (Keyword: details)
-- Other (Keyword: other)
-
-Please respond with a single word keyword that corresponds to the identified topic.
-    """),
+    ("system", Path("./prompts/classification.txt").read_text()
+    ),
     MessagesPlaceholder(variable_name="messages"),
 ])
 
 
 inquiry_response = ChatPromptTemplate.from_messages([
-    ("system", "You are a company AI assistant tasked with drafting responses to customer complaint. "
-        "Generate a professional and empathetic response to the customer's complaint."),
+    ("system", Path("./prompts/benefits.txt").read_text()),
     MessagesPlaceholder(variable_name="messages"),
 ])
 
