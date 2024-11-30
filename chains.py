4f341e8f4728e3189b86ca4887740e1576f143d8
Jan <janpleszynski@gmail.com>
2024-11-30 16:06:31 +0100

    Implement routing.

--- chains.py
+++ chains.py
@@ -11,13 +11,18 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 openai_model = os.getenv("OPENAI_MODEL")
 
 llm = ChatOpenAI(model=openai_model, api_key=openai_api_key)
-subjects = ["process", "benefits", "details", "other"]
+subjects = ["process", "benefits", "details"]
 inquiry_classification = ChatPromptTemplate.from_messages([
     ("system", Path("./prompts/classification.txt").read_text()
     ),
     MessagesPlaceholder(variable_name="messages"),
 ])
 
+
+response_chains_by_subject ={subject: ChatPromptTemplate.from_messages([
+    ("system", Path(f"./prompts/{subject}.txt").read_text()),
+    MessagesPlaceholder(variable_name="messages"),
+]) | llm  for subject in subjects}
 
 inquiry_response = ChatPromptTemplate.from_messages([
     ("system", Path("./prompts/benefits.txt").read_text()),
--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from typing import List, Sequence
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph import END, MessageGraph
-from chains import generate_inquiry_response_chain, generate_classify_inquiry_chain
+from chains import generate_inquiry_response_chain, generate_classify_inquiry_chain, \
+    response_chains_by_subject
+
 
 def is_subject(message):
     return message.content.strip().lower() in ["process", "benefits", "details", "other"]
+
+def get_subject(message):
+    return message.content.strip().lower()
 
 def format_conversation(messages):
     formatted_conversation = []
@@ -21,44 +26,38 @@ def classify_inquiry_node(state: Sequence[BaseMessage]):
     state = [msg for msg in state if not is_subject(msg)]
     return generate_classify_inquiry_chain.invoke({"messages": state})
 
-
-def inquiry_response_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
-    res = generate_inquiry_response_chain.invoke({"messages": messages})
-    return [AIMessage(content=res.content)]
+def inquiry_response_node_factory(subject: str):
+    def inquiry_response_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+        res = response_chains_by_subject[subject].invoke({"messages": messages})
+        return [AIMessage(content=res.content)]
+    return inquiry_response_node
 
 
 def no_applicable_response_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
     return [AIMessage(content="Sorry, we don't have any information about that.")]
 
 
-def should_continue(state: List[BaseMessage]):
-    if state and state[-1].content.strip().lower() == "other":
-        return "OTHER"
-
-    return "RESPOND"
+def select_respond_node(state: List[BaseMessage]):
+    # Last message in the state should contain the subject
+    return get_subject(state[-1])
 
 
 builder = MessageGraph()
 builder.add_node("CLASSIFY", classify_inquiry_node)
-builder.add_node("RESPOND", inquiry_response_node)
-builder.add_node("OTHER", no_applicable_response_node)
+# Build response nodes per subject
+for subject in response_chains_by_subject.keys():
+    builder.add_node(subject, inquiry_response_node_factory(subject))
+
+builder.add_node("other", no_applicable_response_node)
+
 builder.set_entry_point("CLASSIFY")
-
-
-builder.add_conditional_edges("CLASSIFY", should_continue)
-
+builder.add_conditional_edges("CLASSIFY", select_respond_node)
 
 
 with SqliteSaver.from_conn_string("langgraph.db") as checkpointer:
 
     graph = builder.compile(checkpointer=checkpointer)
 
-    inputs = HumanMessage(content="""Give me more benefits""")
+    inputs = HumanMessage(content="""Scoping session?""")
     response = graph.invoke(inputs, config={"thread_id": 42})
     print(format_conversation(messages=response))
-
-# second_input = HumanMessage(content="""
-#     "Do you remember what was initial customer complaint?"
-# """)
-# response = graph.invoke(second_input, config={"thread_id": 42})
-# print(format_conversation(response))
